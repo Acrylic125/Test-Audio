51889f638b64b757a64da0fe7554d8e411e60cd1
Acrylic125 <acrylic125email@gmail.com>
2024-06-15 15:19:07 +0800

    Initial Commit

---
+++ Test Audio/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Test Audio
+//
+//  Created by Benedict Tan on 15/6/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Test Audio/Test_AudioApp.swift
@@ -0,0 +1,17 @@
+//
+//  Test_AudioApp.swift
+//  Test Audio
+//
+//  Created by Benedict Tan on 15/6/24.
+//
+
+import SwiftUI
+
+@main
+struct Test_AudioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
