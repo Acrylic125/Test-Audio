aebea3d47dc1c3529d489793b6cb9ba12c8e248a
Acrylic125 <acrylic125email@gmail.com>
2024-06-15 15:28:42 +0800

    First Init

--- Test Audio/ContentView.swift
+++ Test Audio/ContentView.swift
@@ -2,18 +2,54 @@
 //  ContentView.swift
 //  Test Audio
 //
-//  Created by Benedict Tan on 15/6/24.
-//
 
 import SwiftUI
+import AVKit
+import Observation
+
+class SpeechSynthesizer: ObservableObject { // NSObject, AVSpeechSynthesizerDelegate {
+//    @State private var speechSynthesizer: AVSpeechSynthesizer?
+    private let showModalDispatchQueue = DispatchQueue(label: "ShowModalDispatchQueue2", qos: .userInitiated, attributes: .concurrent)
+    let speechSynthesizer = AVSpeechSynthesizer()
+
+//    override init() {
+//        super.init()
+//        speechSynthesizer.delegate = self
+//    }
+
+    public func speak(text: String) {
+        DispatchQueue.global(qos: .default).async {
+            if AVAudioSession.sharedInstance().category != .playback {
+                do {
+                    try AVAudioSession.sharedInstance().setCategory(.playback)
+                } catch {
+                    print(error)
+                }
+            }
+            let speechUtterance = AVSpeechUtterance(string: text)
+            speechUtterance.voice = AVSpeechSynthesisVoice(language: AVSpeechSynthesisVoice.currentLanguageCode())
+
+            self.speechSynthesizer.speak(speechUtterance)
+//            DispatchQueue.main.async {
+//            }
+        }
+    }
+}
 
 struct ContentView: View {
+    @StateObject private var speechSynthesizer = SpeechSynthesizer()
+    @State private var test = 0.0
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Button("Test Audio") {
+                self.speechSynthesizer.speak(text: "Hello World!")
+            }
+            Slider(
+                value: $test,
+                in: 0...100,
+                step: 0.1
+            )
         }
         .padding()
     }
